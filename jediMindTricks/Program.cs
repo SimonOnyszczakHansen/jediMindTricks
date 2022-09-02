88c19a678218cf611be2c15743d6cd9ed2c63356
Simon Onyszczak Hansen <simo401t@o365.celf.dk>
2022-09-02 23:20:15 +0200

    Add project files.

---
+++ jediMindTricks/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace jediMindTricks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> people = new Dictionary<string, int>() { { "Patrick", 17 } };
+            people.Add("Tue", 18);
+            people.Remove("Tue");
+            foreach (KeyValuePair<string, int> pair in people)
+            {
+                Console.WriteLine(pair.Key);
+            }
+        }
+    }
+}
